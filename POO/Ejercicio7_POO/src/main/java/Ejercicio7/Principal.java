9004818931c9f67e52fed092d31cf786800d2d27
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-03-17 21:59:44 -0600

    menu del cliente y opcion de ingresar dinero terminado (parte 2)

--- POO/Ejercicio7_POO/src/main/java/Ejercicio7/Principal.java
+++ POO/Ejercicio7_POO/src/main/java/Ejercicio7/Principal.java
@@ -1,14 +1,96 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Ejercicio7;
 
-/**
- *
- * @author USER
- */
+import java.util.Scanner;
+
+
 public class Principal {
+    
+    public static int buscarNumeroCuenta( Cuenta cuentas[],int n ){
+        
+        int i =0,indice=0;
+        boolean encontrado = false;
+        
+        
+        /* =========== Busqueda Secuencial ================= */
+        while((i <cuentas.length) && (encontrado == false)){
+            if(cuentas[i].getNumeroCuenta() == n){
+                encontrado = true;
+                indice = i;
+            }
+            i++;
+        }
+        if (encontrado == false) {
+            indice = -1;
+        }
+        return indice;
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        String nombre, apellido, dni;
+        int numeroCuenta,nCuentas,opcion, indiceCuenta;
+        double saldo,cantidad;
+
+        //creo los objetos
+        Cuenta cuentas[];
+        Cliente cliente;
+        
+        System.out.println("Digite el nombre del cliente: ");
+        nombre = entrada.nextLine();
+        System.out.println("Digite el apellido del cliente: ");
+        apellido = entrada.nextLine();
+        System.out.println("Digite el DNI del cliente: ");
+        dni = entrada.nextLine();
+        System.out.println("Ahora, digite cuentas tiene: ");
+        nCuentas = entrada.nextInt();
+        
+        
+        cuentas = new Cuenta[nCuentas];
+        
+        for(int i=0; i<cuentas.length; i++){
+            System.out.println("\nDigite los datos para la cuenta"+ (i+1) +":");
+            System.out.println("Digite el numero de cuenta: ");
+            numeroCuenta = entrada.nextInt();
+            System.out.println("Digite el saldo de la cuenta: ");
+            saldo = entrada.nextDouble();
+            
+            /*========== inicializo las cuentas del cliente ==================*/
+            cuentas[i] = new Cuenta(numeroCuenta, saldo);
+        }
+        
+        cliente = new Cliente(nombre, apellido, dni, cuentas);
+        
+        do{
+            System.out.println("\n.:MENU:.");
+            System.out.println("1. Ingresar dinero en la cuenta");
+            System.out.println("2. Retirar dinero de la cuenta");
+            System.out.println("3. Consultar saldo de la cuenta");
+            System.out.println("4. Salir");
+            System.out.println("Digite la opcion del menu");
+            opcion = entrada.nextInt();
+            
+            switch(opcion){
+                case 1: System.out.println("\nDigite el numero de cuenta: ");
+                    numeroCuenta = entrada.nextInt();
+                    indiceCuenta = buscarNumeroCuenta(cuentas, numeroCuenta);
+                    
+                    if (indiceCuenta == -1) {
+                        System.out.println("\nEl numero de cuenta ingresado no Existe!");
+                    }else{
+                        System.out.println("\nDigite la cantidad de dinero a ingresar: ");
+                        cantidad = entrada.nextDouble();
+                        
+                        cliente.ingresar_dinero(indiceCuenta, cantidad);
+                        System.out.println("\nLIngreso realizado correctamente");
+                        System.out.println("Saldo disponible: "+cliente.consultar_saldo(indiceCuenta));
+                    }
+                break;
+            }
+            
+        }while( opcion !=4 );
+        
+    }
     
 }
